a31f43a7785e70d99cb8a4b223e869760d78ba6f
Łukasz Radomiński <lukaszr82@o2.pl>
2022-07-05 13:06:32 +0200

    Add api token

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 from flask import Flask, render_template
+import tmdb_client
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    movies = []
-    return render_template("homepage.html", movies=movies)
+    movies = tmdb_client.get_movies(8) # maksymalnie 8 filmów
+    return render_template("homepage.html", movies=movies) # wyświetla szablon html, w którym zmienna movies przejmuje argumenty zmiennej movies z main.py
+
+
+# pozwala on na wstrzyknięcie do kontekstu każdego szablonu dodatkowych danych lub obiektów, by nie robić tego za każdym razem w widoku
+# Teraz każdy szablon będzie miał dostępny obiekt o nazwie tmdb_image_url, czyli funkcję, która przyjmuje dwa parametry: path oraz size.
+@app.context_processor
+def utility_processor():
+    def tmdb_image_url(path, size):
+        return tmdb_client.get_poster_url(path, size)
+    return {"tmdb_image_url": tmdb_image_url}  # dekorator context proessor zwraca słownik w którym tmdb_image_url to key a wartośc jest funkcją tmdb_image_url
+
+
+
+# moja funkcja ..................
+def get_movie_info():
+    movies =  tmdb_client.get_popular_movies["results"][:8]
+    for titles_dict in movies: 
+        keys = ["title", "backdrop_path"]
+        dict2 = {x:titles_dict[x] for x in keys}
+        return dict2
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- tmdb_client.py
+++ tmdb_client.py
@@ -1,14 +1,15 @@
 import requests
+import logging
 
 # funkcja, która pobierze dane o najpopularniejszych filmach
 # nalezy stworzyć osobny plik, w którym będziemy przechowywać cały kod odpowiedzialny za komunikację z API : tmdb_client.py
 
-def get_popular_movies(): 
-    endpoint = "https://api.themoviedb.org/3/movie/popular"  # wysyłanie zapytania GET na adres ...
-    api_token = "0b80e749eadb9b04444161a5e2c1ce1e" # wysyłanie klucza autoryzujacego  ......
+def get_popular_movies(): # funkcja która zwraca pełną listę popularnych filmów 
+    endpoint = "https://api.themoviedb.org/3/movie/popular"  # adres używany do wysyłania zapytania GET na adres ...
+    api_token ="eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwYjgwZTc0OWVhZGI5YjA0NDQ0MTYxYTVlMmMxY2UxZSIsInN1YiI6IjYyOTA4Y2M5ZWQyYWMyMTZiOTAyOTlkYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AqfzLL-wtdgqzIfh5AjyZvIi7b8yrl7B1MOEyGqJFW8" # klucz autoryzacyjny przechowywany w zmiennej> dzięki temu nie trzeba podawać klucza w  URL, za każdym zapytaniem 
     headers = {
         "Authorization": f"Bearer {api_token}"
-    }
+    }                                           # nagówek Authorization występujący w każdym zapytaniu 
     response = requests.get(endpoint, headers=headers)
     return response.json()
 
@@ -18,6 +19,14 @@ def get_popular_movies():
     # 3/ file_path ( tu jest "poster_api_path" - opis dalej ) - ścieżka do pliku (  "poster_path": "/9Gtg2DzBhmYamXBS1hKAhiwbBKS.jpg"< podawana jest w JSON - który ściągany jest pezez def get_popular_movies(): - 
 
 
-def get_poster_url(poster_api_path, size="w342"): 
+def get_poster_url(poster_api_path, size="w342"): ### ??? JAKIE JEST ŹRÓDŁO "poster_api_path"  ### funkcja, która potrafi stworzyć działający adres do obrazka # MOJE UWAGI: podczas wywoływania funkcji podawany jest tylko argument "poster_api_path" - reszta jest juz w funkcji
     base_url = "https://image.tmdb.org/t/p/"
     return f"{base_url}{size}/{poster_api_path}"
+
+
+# wykorzystywane w main.py > aby funkcja odpowiedzialna za stronę główną, jedynie pobierała odpowiednią liczbę filmów, którą chcemy pokazać
+def get_movies(how_many):  
+    data = get_popular_movies()
+    print("abc",flush=True)
+    print(data,flush=True)
+    return data["results"][:how_many]
