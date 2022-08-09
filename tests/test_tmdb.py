be487c222a38f97d9c6b53df98f0ea7338349d58
Łukasz Radomiński <lukaszr82@o2.pl>
2022-08-09 16:02:00 +0200

    Add one api call

--- tests/test_tmdb.py
+++ tests/test_tmdb.py
@@ -1,6 +1,7 @@
 # import tmdb_client, pytest
 import os
 import tmdb_client 
+from unittest.mock import Mock
 
 def test_get_poster_url_uses_default_size(): # test: czy funkcja używa domyślnego rozmiaru zdjęcia
    # Przygotowanie danych
@@ -15,3 +16,32 @@ def test_get_poster_url_uses_default_size(): # test: czy funkcja używa domyśln
 def test_get_movies_list_type_popular(): # test pobierania danych z API TheMovieDB >> czy dane zostały pobrane 
    movies_list = tmdb_client.get_movies_list(list_type="popular")
    assert movies_list is not None
+
+###  poniżej testy przy użyciu Mock() i monkeypath
+# test 1 
+def some_function_to_mock():
+   raise Exception("Original was called")
+
+def test_mocking(monkeypatch):  # Po pierwsze nasz test zyskał nowy parametr monkeypatch, wykorzystaliśmy w ten sposób wbudowany w pytest mechanizm fixtures. Taki zapis będzie działał jedynie w obrębie testów pisanych z użyciem tej biblioteki. # taki zapis  traktujemy jako "sprytny import" – definiujemy, że nasz test wymaga kodu fixture o nazwie monkeypatch, a pytest to wykryje i uruchomi funkcję testującą z odpowiednim parametrem.
+   my_mock = Mock() # Następnie tworzymy obiekt klasy Mock() z modułu unittest.mock, który służy do "udawania" zachowań innych obiektów. Dzięki bardzo elastycznej konfiguracji można łatwo symulować oczekiwane zachowania w kodzie testowym. Mock() będzie tak naprawdę wszystkim, czym chcemy.
+   my_mock.return_value = 2 # W naszym przypadku przysłonimy całą funkcję przy pomocy naszego obiektu Mock, więc ustawiamy mu pole return_value na wartość, której oczekujemy.  ## Jeśli wywołamy mock jak funkcję, to zostanie zawsze zwrócona zawartość pola return_value. Domyślnie jest ona ustawiona na nowy obiekt Mock().
+   monkeypatch.setattr("tests.test_tmdb.some_function_to_mock", my_mock) # Ostatnia nowość to wywołanie monkeypatch.setattr() – ten fragment jest najważniejszy z punktu widzenia naszych testów. To on odpowiada za finalne przesłonięcie obiektu innym. Funkcja setattr() jako pierwszy argument przyjmuje dokładną ścieżkę do obiektu, który ma zostać przysłonięty. Drugim argumentem natomiast jest obiekt, którym przysłaniamy.
+   result = some_function_to_mock()
+   assert result == 2
+   # Spróbuj teraz uruchomić testy przy pomocy pytest i przekonaj się, czy zobaczysz wyjątek. Jeśli testy przejdą pomyślnie — oznacza to, że przesłanianie obiektów z użyciem Mock zadziałało.
+
+# test 2 
+def test_get_movies_list(monkeypatch):
+   # Lista, która będzie zwracać przysłonięte "zapytanie do API"
+   mock_movies_list = ['Movie 1', 'Movie 2']
+
+   requests_mock = Mock()
+   # Wynik wywołania zapytania do API
+   response = requests_mock.return_value
+   # Przysłaniamy wynik wywołania metody .json()
+   response.json.return_value = mock_movies_list # Nadpisujemy metodę .json() i ustawiamy jej zwracaną wartość jako wcześniej zdefiniowaną listę. # W ten sposób możesz przesłaniać zachowania metod, które są wywoływane na obiekcie Mock().
+   monkeypatch.setattr("tmdb_client.requests.get", requests_mock)
+
+
+   movies_list = tmdb_client.get_movies_list(list_type="popular")
+   assert movies_list == mock_movies_list
--- tmdb_client.py
+++ tmdb_client.py
@@ -5,23 +5,43 @@ API_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwYjgwZTc0OWVhZGI5YjA0NDQ0MTYxYTVlMm
 # funkcja, która pobierze dane o najpopularniejszych filmach
 # nalezy stworzyć osobny plik, w którym będziemy przechowywać cały kod odpowiedzialny za komunikację z API : tmdb_client.py
 
+# JEDNO ZAPYTANIE DO API (z modułu 9,1) do comitu "Add test picture size" włącznie  , funkcja call_tmdb_api nie występowała, a każra inne funkcja pobierała dane zamodzielnie z  oddzielnych API
+# po wprowadzeniu call_tmdb_api w commicie "Add one api call", poniższe funkcje sa mjuż uproszczone hgdyz sięgają do  call_tmdb_api
+
+def call_tmdb_api(endpoint):
+   full_url = f"https://api.themoviedb.org/3/{endpoint}"
+   headers = {
+       "Authorization": f"Bearer {API_TOKEN}"
+   }
+   response = requests.get(full_url, headers=headers)
+   response.raise_for_status()
+   return response.json()
+
+
 def get_popular_movies(): # funkcja która zwraca pełną listę popularnych filmów 
-    endpoint = "https://api.themoviedb.org/3/movie/popular"  # adres używany do wysyłania zapytania GET na adres ...
-    headers = {
-        "Authorization": f"Bearer {API_TOKEN}"
-    }                                           # nagówek Authorization występujący w każdym zapytaniu 
-    response = requests.get(endpoint, headers=headers)
-    return response.json()
+    # endpoint = "https://api.themoviedb.org/3/movie/popular"  # adres używany do wysyłania zapytania GET na adres ...
+    # headers = {
+    #    "Authorization": f"Bearer {API_TOKEN}"
+    # }                                           # nagówek Authorization występujący w każdym zapytaniu 
+    # response = requests.get(endpoint, headers=headers)
+    # return response.json()
+    
+    ## POWYŻEJ wersja przed uproszczeniem  PONIŻEJ wersja po uproszczeniu o funkcję call_tmdb_api
+    return call_tmdb_api(f"movie/popular")
  
 
 def get_movies_list(list_type):
-    endpoint = f"https://api.themoviedb.org/3/movie/{list_type}"
-    headers = {
-        "Authorization": f"Bearer {API_TOKEN}"
-    }
-    response = requests.get(endpoint, headers=headers)
-    response.raise_for_status() # jeśli odpowiedź od API zakończy się czymkolwiek innym niż sukcesem – aplikacja zwróci wyjątek. Dzięki temu możemy mieć pewność, że operujemy tylko i wyłącznie na poprawnych danych pobranych z API
-    return response.json()
+    # endpoint = f"https://api.themoviedb.org/3/movie/{list_type}"
+    # headers = {
+    #    "Authorization": f"Bearer {API_TOKEN}"
+    # }
+    # response = requests.get(endpoint, headers=headers)
+    # response.raise_for_status() # jeśli odpowiedź od API zakończy się czymkolwiek innym niż sukcesem – aplikacja zwróci wyjątek. Dzięki temu możemy mieć pewność, że operujemy tylko i wyłącznie na poprawnych danych pobranych z API
+    # return response.json()
+    
+    ## POWYŻEJ wersja przed uproszczeniem  PONIŻEJ wersja po uproszczeniu o funkcję call_tmdb_api
+    return call_tmdb_api(f"movie/{list_type}")
+
 
 # poniżej funkcja do tworzenia adresu do obrazka; adres  składa się z 3 elementów:
     # 1/ base_url - adres bazowy ( pochodzi z https://developers.themoviedb.org/3/configuration/get-api-configuration), 
@@ -43,26 +63,35 @@ def get_movies(how_many,list_type):
     return data["results"][:how_many] # wywołanie ze słownika  zagnieżdżonego  za pomocą metody: nazwa zmiennej['nazwa klucza'][nr indeksu w  LIŚCIE obiektów]
 
 def get_single_movie(movie_id):
-    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}"
-    headers = {
-        "Authorization": f"Bearer {API_TOKEN}"
-    }
-    response = requests.get(endpoint, headers=headers)
-    return response.json()
+    # endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}"
+    # headers = {
+    #    "Authorization": f"Bearer {API_TOKEN}"
+    # }
+    # response = requests.get(endpoint, headers=headers)
+    # return response.json()
+
+    ## POWYŻEJ wersja przed uproszczeniem  PONIŻEJ wersja po uproszczeniu o funkcję call_tmdb_api
+    return call_tmdb_api(f"movie/{movie_id}")
 
 def get_single_movie_cast(movie_id):
     endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/credits"
     headers = {
-        "Authorization": f"Bearer {API_TOKEN}"
+      "Authorization": f"Bearer {API_TOKEN}"
     }
     response = requests.get(endpoint, headers=headers)
-    return response.json()["cast"]   
+    return response.json()["cast"] 
+
+    ## POWYŻEJ wersja przed uproszczeniem  PONIŻEJ wersja po uproszczeniu o funkcję call_tmdb_api
+    # return call_tmdb_api(f"movie/{movie_id}/credits")  
 
 
 def get_movie_images(movie_id):
-    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/images"
-    headers = {
-        "Authorization": f"Bearer {API_TOKEN}"
-    }
-    response = requests.get(endpoint, headers=headers)
-    return response.json()    
+    # endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/images"
+    # headers = {
+    #    "Authorization": f"Bearer {API_TOKEN}"
+    # }
+    # response = requests.get(endpoint, headers=headers)
+    # return response.json()    
+    
+    ## POWYŻEJ wersja przed uproszczeniem  PONIŻEJ wersja po uproszczeniu o funkcję call_tmdb_api
+    return call_tmdb_api(f"movie/{movie_id}/images")  
